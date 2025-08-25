60bda4fac777bdd6c1ca3fc427643f07770f9d98
ASproson <a.sproson@outlook.com>
2025-08-25 09:55:12 +0100

    Grasping basics

--- src/extension.ts
+++ src/extension.ts
@@ -13,13 +13,23 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('custom-pomodoro.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('custom-pomodoro.hiWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Custom Pomodoro!');
+		vscode.window.showInformationMessage('Hi from Custom Pomodoro!');
+	});
+
+	const dis = vscode.commands.registerCommand('custom-pomodoro.time', () => {
+		const date = new Date();
+		const hour = String(date.getHours() + 1).padStart(2, '0');
+		const min = String(date.getMinutes()).padStart(2, '0');
+
+		vscode.window.showInformationMessage(`Current time is ${hour}:${min}`);
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(dis);
+
 }
 
 // This method is called when your extension is deactivated
